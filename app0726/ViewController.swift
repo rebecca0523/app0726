2ccfa571a2faad9b2016fc431d82af20c1078520
YaChin Li <selina66@ms68.hinet.net>
2018-07-26 19:26:18 +0800

    Initial Commit

---
+++ app0726/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app0726
+//
+//  Created by Ya Chin Lee on 2018/7/26.
+//  Copyright © 2018年 Ya Chin Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
